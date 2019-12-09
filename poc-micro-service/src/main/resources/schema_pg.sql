9434eed692aadfc64b965b9fedfacc095403390b
anand <anand.gv268@gmail.com>
2019-12-09 14:05:43 -0500

    local data

--- poc-micro-service/src/main/resources/schema_pg.sql
+++ poc-micro-service/src/main/resources/schema_pg.sql
@@ -1,9 +1,6 @@
-﻿drop table tbl_user;
-
 CREATE TABLE IF NOT EXISTS tbl_user (
   id SERIAL PRIMARY KEY ,
   username varchar(25) NOT NULL,
-  username1 varchar(75),
   password varchar(75) NOT NULL,
   salary int NOT NULL,
   age int NOT NULL
